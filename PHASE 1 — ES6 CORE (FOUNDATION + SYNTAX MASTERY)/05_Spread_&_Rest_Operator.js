3230683a56a9672686720476b9814432f581d720
codewithdevu <divyanshyadav8302@gmail.com>
2026-01-07 22:08:44 +0530

    add the file

---
+++ PHASE 1 — ES6 CORE (FOUNDATION + SYNTAX MASTERY)/05_Spread_&_Rest_Operator.js
@@ -0,0 +1,23 @@
+// rest and spread opreator
+
+// Example Rest 
+
+function addnum(a,b,c, ...other ) {
+    console.log(other);
+    return a+b+c+ other.reduce((sum, num) => sum + num, 0);
+}
+
+const result = addnum(2,3,4,5,6,7,8);
+console.log(result);
+
+// spread
+
+let names = ["divyansh" ,"hemant" , "ranveeer" ]
+
+function getnames(name1,name2,name3){
+    console.log(name1,name2,name3);
+}
+
+getnames(names[0],names[1],names[2]);
+getnames(...names);
+getnames(names)
