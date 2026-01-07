f8b9ad9967d3895299bbd74ca31e2105bf522ff3
codewithdevu <divyanshyadav8302@gmail.com>
2026-01-07 22:38:29 +0530

    add some codes file

--- PHASE 1 — ES6 CORE (FOUNDATION + SYNTAX MASTERY)/05_Spread_&_Rest_Operator.js
+++ PHASE 1 — ES6 CORE (FOUNDATION + SYNTAX MASTERY)/05_Spread_&_Rest_Operator.js
@@ -41,3 +41,36 @@ let newstudent = {
 }
 console.log(newstudent);
 
+// (A) Copy vs Reference (Very Important)\
+
+// old method
+// const arr1 = [1, 2, 3];
+// const arr2 = arr1;
+
+// arr2.push(4);
+// console.log(arr1); 
+// console.log(arr2);
+
+// ES6 method
+const arr1 = [1,2,3];
+const arr2 = [...arr1];
+
+arr2.push(4,5);
+console.log(arr2);
+console.log(arr1); 
+
+// (B) Merging Arrays
+
+const a = [1, 2];
+const b = [3, 4];
+
+const merged = [...a, ...b];
+console.log(merged); 
+
+// (C) Merging Objects
+
+const user = { name: "A", age: 20 };
+const extra = { city: "Delhi" };
+
+const mergedUser = { ...user, ...extra };
+console.log(mergedUser);
