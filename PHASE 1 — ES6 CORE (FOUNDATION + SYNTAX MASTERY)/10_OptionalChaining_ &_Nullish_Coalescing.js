e0e14db1464e5ebe0d767e660e41ce98d8c30e0c
codewithdevu <divyanshyadav8302@gmail.com>
2026-01-11 15:16:05 +0530

    add the file

--- PHASE 1 — ES6 CORE (FOUNDATION + SYNTAX MASTERY)/10_OptionalChaining_ &_Nullish_Coalescing.js
+++ PHASE 1 — ES6 CORE (FOUNDATION + SYNTAX MASTERY)/10_OptionalChaining_ &_Nullish_Coalescing.js
@@ -2,9 +2,29 @@
 
 // in javasript, the following values are considered falsy:  false, 0 , "" (an empty string ) , null , undefined , and NAN.
 
+
+// Nullish Coalescing (??)
+
 let myval = 0;
 const mynumber = myval || 10 ;
 const mynumber2 = myval ?? 10 ;
 
 console.log("~ file:  nullish.js:5 ~MYname1: " , mynumber);
 console.log("~ file:  nullish.js:6 ~MYname2: " , mynumber2);
+
+// Optional Chaining (?.)
+
+// optional chaining is a feature in javasript that allows you to acess properties of an object or elements of an array  without having to check whether the object or array is null or undefined first. it is repreesented by the ?.  operator and can be used to concisely access deeply nested properties without having to write a long chain of id if statements to check for null or undefined values.
+
+const user = {
+    name: "thapa",
+    age: 28,
+    address: {
+        street: "mian st",
+        city: "new road",
+        state: "rajasthan",
+        zip: 10101,
+    },
+};
+
+console.log(user.address.housenumber?.roadnumber);
